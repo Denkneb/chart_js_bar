9290638f86bc4a829e1cb3abda20c3c1394e4838
dkislinskiy <denkneb@gmail.com>
2017-08-10 13:36:48 +0700

    create models

--- proceeds/models.py
+++ proceeds/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Locomotive(models.Model):
+    series = models.CharField(max_length=10)
+    rate = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return '%s' % self.series
+
+
+class Filial(models.Model):
+    name = models.CharField(max_length=100)
+    locomotive = models.ManyToManyField(Locomotive, blank=True)
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Year(models.Model):
+    year = models.CharField(max_length=10)
+
+    def __str__(self):
+        return '%s' % self.year
+
+
+class Mileage(models.Model):
+    mileage = models.DecimalField(max_digits=20, decimal_places=10)
+    year = models.ForeignKey(Year)
+    locomotive = models.ForeignKey(Locomotive)
+    filial = models.ForeignKey(Filial)
+
+    def __str__(self):
+        return '%s' % self.locomotive
