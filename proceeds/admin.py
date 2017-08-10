4d9c265fdcdb8a8b0541663ac6c9e4fa018d10ac
dkislinskiy <denkneb@gmail.com>
2017-08-10 13:37:52 +0700

    register models

--- proceeds/admin.py
+++ proceeds/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from proceeds.models import Locomotive, Filial, Mileage, Year
 
-# Register your models here.
+
+@admin.register(Locomotive)
+class LocomotiveAdmin(admin.ModelAdmin):
+    fields = ('series', 'rate')
+
+
+@admin.register(Filial)
+class FilialAdmin(admin.ModelAdmin):
+    fields = ('name', 'locomotive')
+
+
+@admin.register(Mileage)
+class MileageAdmin(admin.ModelAdmin):
+    fields = ('mileage', 'year', 'locomotive', 'filial')
+
+
+@admin.register(Year)
+class YearAdmin(admin.ModelAdmin):
+    fields = ('year',)
